98ac73d12fd7b980d855924d6cb5489f80c428ba
Deamons64577 <168561580+Deamons64577@users.noreply.github.com>
2025-02-04 23:20:23 +0100

    inventaire
    Le debut de l'inventaire

---
+++ Projet Unity/Assets/Scripts/Romario/Inventaire.cs
@@ -0,0 +1,7 @@
+﻿namespace Romario
+{
+    public class Inventaire
+    {
+        
+    }
+}
---
+++ Projet Unity/Assets/Scripts/Romario/Item_Scipt_RL.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Items",menuName = "Items/New Items")]
+public class Item_Scipt_RL : ScriptableObject
+{
+   public string name;
+   public GameObject prefab;
+}
