5e7c57fa2be00170bd843f7e9f4eb8e1f16b84f6
Deamons64577 <168561580+Deamons64577@users.noreply.github.com>
2025-02-18 10:32:21 +0100

    sauvegarde
    au cas je peux plus le faire

--- Projet Unity/Assets/Scripts/Romario/PickUpItem_RL.cs
+++ Projet Unity/Assets/Scripts/Romario/PickUpItem_RL.cs
@@ -3,26 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+using UnityEngine;
+
 public class PickUpItem_RL : MonoBehaviour
 {
-
-    [SerializeField] private float pickUpRange = 2.6f;
-    private float spherRaduis = 0.5f;
-    private float detectionDist = 10f;
+    [SerializeField] private float pickUpRange = 2.6f; // Distance de ramassage
     public LayerMask detectionLayer;
-
     public Inventaire_RL inventaire;
+
     void Update()
     {
+       
+     
+
+        // Lancer un Raycast
         RaycastHit hit;
-        if (Physics.SphereCast(transform.position,spherRaduis,transform.forward,out hit,detectionDist,detectionLayer))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, pickUpRange))
         {
-           
-                Debug.Log("Tu pointe l'item");
-                if (Input.GetKeyDown(KeyCode.E))
+            Debug.Log("Tu pointes l'item : " + hit.transform.name);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // Vérifier si l'objet a bien un composant Item_RL
+                if (hit.transform.CompareTag("Item"))
                 {
-                   inventaire.content.Add(hit.transform.gameObject.GetComponent<Item_RL>().item);
+                    if (hit.transform.gameObject.GetComponent<Item_RL>().item != null)
+                    {
+                        inventaire.content.Add(hit.transform.gameObject.GetComponent<Item_RL>().item);
+                        Destroy(hit.transform.gameObject);
+                        Debug.Log("Objet ramassé : ");
+                    }
                 }
+                else
+                {
+                    Debug.Log("L'objet pointé n'a pas de script Item_RL !");
+                }
+            }
         }
+        else
+        {
+            Debug.Log("Aucun objet détecté.");
+        }
+
+        // Dessiner la ligne pour voir le Raycast
+        Debug.DrawRay(transform.position, transform.forward* pickUpRange, Color.green);
     }
 }
