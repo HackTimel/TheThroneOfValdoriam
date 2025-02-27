5d26b530fe998fd13924d969d266c9263d20c8e9
Deamons64577 <168561580+Deamons64577@users.noreply.github.com>
2025-02-27 23:59:19 +0100

    IA partie 2
    -L'IA a une animation d'iDLe et de run fontionnelle
    Il reste a implemnter l'attaque

--- Projet Unity/Assets/Scripts/Romario/EnemyIA.cs
+++ Projet Unity/Assets/Scripts/Romario/EnemyIA.cs
@@ -1,15 +1,34 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 
 public class EnemyIA : MonoBehaviour
 {
-    [Header("Reference")] [SerializeField] public Transform player;
-
-    [SerializeField] public NavMeshAgent agent;
+    [Header("Reference")]
+    [SerializeField] public Transform player;
+    [SerializeField]public NavMeshAgent agent;
+    [SerializeField] private Animator animator;
+    [Header("Stat")]
+    [SerializeField] private float detection_radius;
+    [SerializeField] private bool awaiting_distance;
+    [SerializeField] private float vitesse;
+        
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.position);
+        if (Vector3.Distance(player.position,transform.position)<detection_radius)
+        {
+            agent.speed = vitesse;
+            agent.SetDestination(player.position);
+        }
+        animator.SetFloat("Speed",agent.velocity.magnitude);
+       
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detection_radius);
     }
 }
