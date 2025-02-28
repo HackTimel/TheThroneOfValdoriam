8d9d287b6b7887ed999a94c0d7dbfda4e8ee0758
Romain <romainb2005@gmail.com>
2025-02-28 18:01:21 +0100

    boutons qui s'enlèvent

--- Projet Unity/Assets/Scenes/MULTI_test/TutorialManager.cs
+++ Projet Unity/Assets/Scenes/MULTI_test/TutorialManager.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
+    public GameObject uiCanvas;
+
     public void StartClient(){
         NetworkManager.Singleton.StartClient();
+		if (uiCanvas != null)
+        {
+            uiCanvas.SetActive(false); // Cache le Canvas
+        }
     }
 
     public void StartHost(){
         NetworkManager.Singleton.StartHost();
+		if (uiCanvas != null)
+        {
+            uiCanvas.SetActive(false); // Cache le Canvas
+        }
     }
 }
