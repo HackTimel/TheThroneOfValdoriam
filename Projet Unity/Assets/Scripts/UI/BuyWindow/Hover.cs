38b4298b719c3a600c1f73ee72a0debf5c78140f
Wackzz <steinmetzleewen@gmail.com>
2025-04-08 22:11:59 +0200

    Before Unpacking
    debug avant commit principal

--- Projet Unity/Assets/Scripts/UI/BuyWindow/Hover.cs
+++ Projet Unity/Assets/Scripts/UI/BuyWindow/Hover.cs
@@ -12,8 +12,32 @@ public class Hover : MonoBehaviour
     public GameObject filtre;
     public int debug;
 
-    /*private void Update()
+    private void Update() //regarde ou est la souris, 
     {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+        
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, results);
+        
+        bool isHovering = false;
+        
+        foreach (RaycastResult result in results) //compare avec les éléments d'UI
+        {
+            if (result.gameObject == gameObject || result.gameObject.transform.IsChildOf(transform)) //si la souris est sur notre element souhaitant être detect, alors faire :
+            {
+                isHovering = true;
+                break;
+            }
+        }
+
+        filtre.SetActive(isHovering);
+    }
+    
+    //c'est si complexe urrrrg
+    /*
         if (EventSystem.current.IsPointerOverGameObject())
         {
             filtre.SetActive(true);
@@ -22,9 +46,10 @@ public class Hover : MonoBehaviour
         {
             filtre.SetActive(false);
         }
-    }
+    } 
     */
-    public void OnPointerEnter(PointerEventData eventData)
+    
+   /* public void OnPointerEnter(PointerEventData eventData)
     {
         filtre.SetActive(true);
     }
@@ -33,4 +58,6 @@ public class Hover : MonoBehaviour
     {
         filtre.SetActive(false);
     }
+    */
+  
 }
