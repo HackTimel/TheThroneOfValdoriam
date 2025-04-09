470287a20fc159991cd95aaa27cdc7f2be091e0d
Wackzz <steinmetzleewen@gmail.com>
2025-04-10 00:08:53 +0200

    minor_fix/typo

---
+++ Projet Unity/Assets/Scripts/UI/BuyWindow/Call methods/ZoneCall.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace UI.BuyWindow.Call_methods
+{
+    public class ZoneCall : MonoBehaviour
+    {
+        public GameObject Windows; //la page de magasin que l'on veut afficher
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player")) //le joueur est entré dans la zone 
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                Windows.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                Windows.SetActive(false);
+            }
+        }
+    }
+}
