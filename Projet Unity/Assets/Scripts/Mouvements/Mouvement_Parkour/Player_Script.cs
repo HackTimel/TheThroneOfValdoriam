bd00f0813c4d6b527a7196e2572592eae29d0604
Deamons64577 <168561580+Deamons64577@users.noreply.github.com>
2025-04-11 16:57:26 +0200

    Changement

--- Projet Unity/Assets/Scripts/Mouvements/Mouvement_Parkour/Player_Script.cs
+++ Projet Unity/Assets/Scripts/Mouvements/Mouvement_Parkour/Player_Script.cs
@@ -7,6 +7,17 @@ public class Player_Script : MonoBehaviour
 {
     [Header("Player Movement")] public float movement_speed = 4f;
     public MainCameraController MCC;
+    public float rotationSpeed;
+    public Quaternion requirerot;
+    [Header("Player Animator")]
+    public Animator animator;
+    [Header("Player Conllison")]
+    public CharacterController controller;
+
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     private void Update()
     {
@@ -17,14 +28,18 @@ public class Player_Script : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        float movementAmount = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+        float movementAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
         var movementInput = new Vector3(horizontal,0, vertical).normalized;
         var movementDirection = MCC.flatRotation * movementInput;
         if (movementAmount>0)
         {
-            transform.position+=movementInput*movement_speed*Time.deltaTime;
-            transform.rotation = Quaternion.LookRotation(movementInput);
+           controller.Move(movementDirection * movement_speed * Time.deltaTime);
+            requirerot = Quaternion.LookRotation(movementDirection);
+
         }
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, requirerot, 
+            rotationSpeed * Time.deltaTime);
+        animator.SetFloat("MovementValue",movementAmount,0.2f,Time.deltaTime);
        
     }
 }
