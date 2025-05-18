52123ca0675d4b1862144993f2d0b8df88d1977a
Wackzz <steinmetzleewen@gmail.com>
2025-05-18 23:33:51 +0200

    début du niveau 1 (tuto)
    a ajouter : des ennemis et des villageois recrutable, et complexifier le tuto avec un hit sur l'ennemi (voir sa mort) pour informer le tuto qu'on sait se battre

---
+++ Projet Unity/Assets/Scripts/UI/Tutorial/Level1.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Level1 : MonoBehaviour
+{
+    public GameObject Panel;
+    
+    public TextMeshProUGUI instructions;
+
+    public int compteur;
+    public bool First_Isover = false;
+    
+    public bool last_Isover = false;
+
+    public int instrcution_index;
+    public string[] liste_tuto =
+    {
+        "Pour avancer, appuie sur la touche z",
+        "Super, maintenant recule avec s",
+        "Ensuite à droite avec d",
+        "Et enfin à gauche avec g",
+        "Devant toi, cette chose... c'est un ennemi, attaque le avec clic gauche !",
+        "Bon, on dirait que j'ai plus besoin de t'apprendre quoique ce soit, libère les villagois, et bon courage..."
+    };
+
+    public string next_instruction
+    {
+        get
+        {
+            if (instrcution_index < liste_tuto.Length - 1)
+            {
+                instrcution_index++;
+                return liste_tuto[instrcution_index];
+            }
+            return "Il semblerait que je n'ai plus rien à t'expliquer";
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!First_Isover)
+        {
+            compteur++;
+            if (compteur % 900 == 0)
+            {
+                First_Isover = true;
+                instructions.text = liste_tuto[0];
+                instrcution_index = 0;
+                compteur = 0;
+            }
+        }
+
+        if (instrcution_index == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                instructions.text = next_instruction;
+            }
+        }
+
+        if (instrcution_index == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                instructions.text = next_instruction;
+            }
+        }
+        if (instrcution_index == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                instructions.text = next_instruction;
+            }
+        }
+        if (instrcution_index == 3)
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                instructions.text = next_instruction;
+            }
+        }
+        if (instrcution_index == 4)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                instructions.text = next_instruction;
+            }
+        }
+
+        if (instrcution_index == 5)
+        {
+            if (!last_Isover)
+            {
+                compteur++;
+                if (compteur % 900 == 0)
+                {
+                    last_Isover = true;
+                    Panel.SetActive(false);
+                }
+            }
+        }
+        
+    }
+    
+    
+    
+}
