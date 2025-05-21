fc8ae94bfe9c7f444dfa756556aa855c34f2bc5c
Deamons64577 <168561580+Deamons64577@users.noreply.github.com>
2025-05-21 16:48:15 +0200

    upadate map

---
+++ Projet Unity/Assets/Scripts/Quets2/Minimap.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public Transform player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
